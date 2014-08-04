3c799aa13d8301320cbce9f878cd04ad1cea6e31
Jimmy Hickey <jameshickey@me.com>
2014-08-04 17:47:16 -0400

    Fix: Get username and profile url for tw and instagram

--- hybridauth/Hybrid/Providers/Instagram.php
+++ hybridauth/Hybrid/Providers/Instagram.php
@@ -6,52 +6,54 @@
 */
 
 /**
-* Hybrid_Providers_Instagram (By Sebastian Lasse - https://github.com/sebilasse)
-*/
+ * Hybrid_Providers_Instagram (By Sebastian Lasse - https://github.com/sebilasse)
+ */
 class Hybrid_Providers_Instagram extends Hybrid_Provider_Model_OAuth2
-{ 
-	// default permissions   
-	public $scope = "basic"; 
+{
+    // default permissions
+    public $scope = "basic";
 
-	/**
-	* IDp wrappers initializer 
-	*/
-	function initialize()
-	{
-		parent::initialize();
+    /**
+     * IDp wrappers initializer
+     */
+    function initialize() {
+        parent::initialize();
 
-		// Provider api end-points
-		$this->api->api_base_url  = "https://api.instagram.com/v1/";
-		$this->api->authorize_url = "https://api.instagram.com/oauth/authorize/";
-		$this->api->token_url     = "https://api.instagram.com/oauth/access_token";
-	}
+        // Provider api end-points
+        $this->api->api_base_url = "https://api.instagram.com/v1/";
+        $this->api->authorize_url = "https://api.instagram.com/oauth/authorize/";
+        $this->api->token_url = "https://api.instagram.com/oauth/access_token";
+    }
 
-	/**
-	* load the user profile from the IDp api client
-	*/
-	function getUserProfile(){ 
-		$data = $this->api->api("users/self/" ); 
+    /**
+     * load the user profile from the IDp api client
+     */
+    function getUserProfile() {
+        $data = $this->api->api("users/self/");
 
-		if ( $data->meta->code != 200 ){
-			throw new Exception( "User profile request failed! {$this->providerId} returned an invalid response.", 6 );
-		}
+        if ($data->meta->code != 200) {
+            throw new Exception("User profile request failed! {$this->providerId} returned an invalid response.", 6);
+        }
 
-		$this->user->profile->identifier  = $data->data->id;
+        $this->user->profile->identifier = $data->data->id;
 
-    $counts = (array) $data->data->counts;
+        $counts = (array)$data->data->counts;
 
-    $this->user->profile->reach = $counts["followed_by"];
+        $this->user->profile->reach = $counts["followed_by"];
 
-		$this->user->profile->displayName = $data->data->full_name ? $data->data->full_name : $data->data->username;
-    $nameBreak = explode(" ",$this->user->profile->displayName);
-    $this->user->profile->firstName = $nameBreak[0];
-    if (count($nameBreak) > 1)
-      $this->user->profile->lastName = $nameBreak[1];
-    $this->user->profile->description = $data->data->bio;
-		$this->user->profile->photoURL    = $data->data->profile_picture;
+        $this->user->profile->displayName = $data->data->full_name ? $data->data->full_name : $data->data->username;
+        $nameBreak = explode(" ", $this->user->profile->displayName);
+        $this->user->profile->firstName = $nameBreak[0];
+        $this->user->profile->userName = $data->data->username;
+        $this->user->profile->profileURL = "https://instagram.com/" . $data->data->username;
+        if (count($nameBreak) > 1) {
+            $this->user->profile->lastName = $nameBreak[1];
+        }
+        $this->user->profile->description = $data->data->bio;
+        $this->user->profile->photoURL = $data->data->profile_picture;
 
-		$this->user->profile->webSiteURL  = $data->data->website; 
+        $this->user->profile->webSiteURL = $data->data->website;
 
-		return $this->user->profile;
-	}
+        return $this->user->profile;
+    }
 }
