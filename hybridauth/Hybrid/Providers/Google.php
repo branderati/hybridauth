a0da2225a4b2d0c899e8078aeb92fd8113feb9c2
Jimmy Hickey <jameshickey@me.com>
2014-08-05 10:24:16 -0400

    Upgrade to latest

--- hybridauth/Hybrid/Providers/Google.php
+++ hybridauth/Hybrid/Providers/Google.php
@@ -2,7 +2,7 @@
 /*!
 * HybridAuth
 * http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
-* (c) 2009-2012, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html 
+* (c) 2009-2014, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html 
 */
 
 /**
@@ -16,7 +16,7 @@ class Hybrid_Providers_Google extends Hybrid_Provider_Model_OAuth2
 	// or here: http://discovery-check.appspot.com/ (unofficial but up to date)
 
 	// default permissions 
-	public $scope = "https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email https://www.google.com/m8/feeds/";
+	public $scope = "https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read https://www.google.com/m8/feeds/";
 
 	/**
 	* IDp wrappers initializer 
@@ -66,8 +66,8 @@ class Hybrid_Providers_Google extends Hybrid_Provider_Model_OAuth2
 		$this->refreshToken();
 
 		// ask google api for user infos
-		if (strpos($this->scope, '/auth/userinfo.email') !== false) {
-			$verified = $this->api->api( "https://www.googleapis.com/oauth2/v2/userinfo" );
+		if (strpos($this->scope, '/auth/plus.profile.emails.read') !== false) {
+			$verified = $this->api->api( "https://www.googleapis.com/plus/v1/people/me" );
 
 			if ( ! isset( $verified->id ) || isset( $verified->error ) )
 				$verified = new stdClass();
@@ -87,7 +87,7 @@ class Hybrid_Providers_Google extends Hybrid_Provider_Model_OAuth2
 		$this->user->profile->firstName     = (property_exists($response,'name'))?$response->name->givenName:"";
 		$this->user->profile->lastName      = (property_exists($response,'name'))?$response->name->familyName:"";
 		$this->user->profile->displayName   = (property_exists($response,'displayName'))?$response->displayName:"";
-		$this->user->profile->photoURL      = (property_exists($response,'image'))?((property_exists($response->image,'url'))?$response->image->url:''):'';
+		$this->user->profile->photoURL      = (property_exists($response,'image'))?((property_exists($response->image,'url'))?substr($response->image->url, 0, -2)."200":''):'';
 		$this->user->profile->profileURL    = (property_exists($response,'url'))?$response->url:"";
 		$this->user->profile->description   = (property_exists($response,'aboutMe'))?$response->aboutMe:"";
 		$this->user->profile->gender        = (property_exists($response,'gender'))?$response->gender:""; 
@@ -110,10 +110,6 @@ class Hybrid_Providers_Google extends Hybrid_Provider_Model_OAuth2
 		$this->user->profile->country 		= (property_exists($response,'country'))?$response->country:"";
 		$this->user->profile->region 		= (property_exists($response,'region'))?$response->region:"";
 		$this->user->profile->zip	 		= (property_exists($response,'zip'))?$response->zip:"";
-
-    $contacts = self::getUserContacts();
-    $this->user->profile->reach = count($contacts);
-
 		if( property_exists($response,'placesLived') ){
 			$this->user->profile->city 	= ""; 
 			$this->user->profile->address	= ""; 
@@ -161,7 +157,7 @@ class Hybrid_Providers_Google extends Hybrid_Provider_Model_OAuth2
 		} else {
 			$this->user->profile->birthDay=0;$this->user->profile->birthMonth=0;$this->user->profile->birthYear=0;
 		}
-		
+                
 		return $this->user->profile;
 	}
 
@@ -187,44 +183,46 @@ class Hybrid_Providers_Google extends Hybrid_Provider_Model_OAuth2
 	
 			if( ! $response ){
 				return ARRAY();
-			}		
-	
-			foreach( $response->feed->entry as $idx => $entry ){
-				$uc = new Hybrid_User_Contact();
-				$uc->email 			= isset($entry->{'gd$email'}[0]->address) ? (string) $entry->{'gd$email'}[0]->address : ''; 
-				$uc->displayName 	= isset($entry->title->{'$t'}) ? (string) $entry->title->{'$t'} : '';  
-				$uc->identifier		= ($uc->email!='')?$uc->email:'';
-				$uc->description 	= '';
-				if( property_exists($entry,'link') ){
-					/**
-					 * sign links with access_token
-					 */
-					if(is_array($entry->link)){
-						foreach($entry->link as $l){
-							if( property_exists($l,'gd$etag') && $l->type=="image/*"){
-								$uc->photoURL = $this->addUrlParam($l->href, array('access_token' => $this->api->access_token));
-							} else if($l->type=="self"){
-								$uc->profileURL = $this->addUrlParam($l->href, array('access_token' => $this->api->access_token));
+			}
+
+			if (isset($response->feed->entry)) {
+				foreach( $response->feed->entry as $idx => $entry ){
+					$uc = new Hybrid_User_Contact();
+					$uc->email 			= isset($entry->{'gd$email'}[0]->address) ? (string) $entry->{'gd$email'}[0]->address : '';
+					$uc->displayName 	= isset($entry->title->{'$t'}) ? (string) $entry->title->{'$t'} : '';
+					$uc->identifier		= ($uc->email!='')?$uc->email:'';
+					$uc->description 	= '';
+					if( property_exists($entry,'link') ){
+						/**
+						 * sign links with access_token
+						 */
+						if(is_array($entry->link)){
+							foreach($entry->link as $l){
+								if( property_exists($l,'gd$etag') && $l->type=="image/*"){
+									$uc->photoURL = $this->addUrlParam($l->href, array('access_token' => $this->api->access_token));
+								} else if($l->type=="self"){
+									$uc->profileURL = $this->addUrlParam($l->href, array('access_token' => $this->api->access_token));
+								}
 							}
 						}
+					} else {
+						$uc->profileURL = '';
 					}
-				} else {
-					$uc->profileURL = '';
-				}
-				if( property_exists($response,'website') ){
-					if(is_array($response->website)){
-						foreach($response->website as $w){
-							if($w->primary == true) $uc->webSiteURL = $w->value;
+					if( property_exists($response,'website') ){
+						if(is_array($response->website)){
+							foreach($response->website as $w){
+								if($w->primary == true) $uc->webSiteURL = $w->value;
+							}
+						} else {
+							$uc->webSiteURL = $response->website->value;
 						}
 					} else {
-						$uc->webSiteURL = $response->website->value;
+						$uc->webSiteURL = '';
 					}
-				} else {
-					$uc->webSiteURL = '';
+
+					$contacts[] = $uc;
 				}
-	
-				$contacts[] = $uc;
-			}  
+			}
 		}
 		
 		// Google social contacts
--- hybridauth/config.php
+++ hybridauth/config.php
@@ -1,8 +1,8 @@
 <?php
-/*!
+/**
 * HybridAuth
 * http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
-* (c) 2009-2012, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
+* (c) 2009-2014, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
 */
 
 // ----------------------------------------------------------------------------------------
@@ -50,11 +50,6 @@ return
 				"keys"    => array ( "id" => "", "secret" => "" ) 
 			),
 
-			"MySpace" => array ( 
-				"enabled" => true,
-				"keys"    => array ( "key" => "", "secret" => "" ) 
-			),
-
 			"LinkedIn" => array ( 
 				"enabled" => true,
 				"keys"    => array ( "key" => "", "secret" => "" ) 
@@ -66,8 +61,12 @@ return
 			),
 		),
 
-		// if you want to enable logging, set 'debug_mode' to true  then provide a writable file by the web server on "debug_file"
+		// If you want to enable logging, set 'debug_mode' to true.
+		// You can also set it to
+		// - "error" To log only error messages. Useful in production
+		// - "info" To log info and error messages (ignore debug messages) 
 		"debug_mode" => false,
 
+		// Path to file writable by the web server. Required if 'debug_mode' is not false
 		"debug_file" => "",
 	);
